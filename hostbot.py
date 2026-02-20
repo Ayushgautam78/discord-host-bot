c7f7fbc88906cb6b051d8ef038dd37c89354f9a6
Ayushgautam78 <99888588+Ayushgautam78@users.noreply.github.com>
2026-02-20 10:21:21 +0530

    Update hostbot.py

--- hostbot.py
+++ hostbot.py
@@ -36,7 +36,7 @@ evening_end_sent = False
 # ===== WEEKLY EVENTS =====
 WEEKLY_REMINDERS = {
 "monday": "**Super Monday**\n\n**Dawn || n/a ||**\n\n**Sentient || <t:1771603244:t> geoguesser ||**\n\n**PrismaX || n/a ||**",
-"tuesday": "**Tuesday**\n\n**Sentient || smashkart <t:1771603244:t>  ||**\n\n**Dawn || n/a ||**\n\n**PrismaX || trivia tango 8:00 PM IST ||**",
+"tuesday": "**Tuesday**\n\n**Sentient || smashkart <t:1771603244:t>  ||**\n\n**Dawn || n/a ||**\n\n**PrismaX || trivia tango <t:1771597845:t> ||**",
 "wednesday": "**Wednesday**\n\n**Sentient || kirka <t:1771603244:t> ||**\n\n**PrismaX || not applicable ||**\n\n**Dawn || n/a ||**",
 "thursday": "**Thursday**\n\n**Sentient || rebus puzzle <t:1771603244:t> ||**\n\n**PrismaX || fun mode 7:30 PM IST ||**\n\n**Dawn || n/a ||**",
 "friday": "**Friday**\n\n**Sentient || among us <t:1771603244:t> ||**\n\n**PrismaX || content clinic 7:30 PM IST ||**\n\n**Dawn || sunray ceremony 8:30 PM IST ||**",
